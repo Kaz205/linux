2a73ea6038cc71dfb919e97d58452b15042a611f
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2025-07-09 19:12:47 +0200

    PM: sleep: Call pm_restore_gfp_mask() after dpm_resume()
    Commit 12ffc3b1513e ("PM: Restrict swap use to later in the suspend
    sequence") changed two pm_restore_gfp_mask() calls in enter_state()
    and hibernation_restore() into one pm_restore_gfp_mask() call in
    dpm_resume_end(), but it put that call before the dpm_resume()
    invocation which is too early (some swap-backing devices may not be
    ready at that point).
    
    Moreover, this code ordering change was not even mentioned in the
    changelog of the commit mentioned above.
    
    Address this by moving that call after the dpm_resume() one.
    
    Fixes: 12ffc3b1513e ("PM: Restrict swap use to later in the suspend sequence")
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Link: https://patch.msgid.link/2797018.mvXUDI8C0e@rjwysocki.net

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -1236,7 +1236,6 @@ void dpm_complete(pm_message_t state)
  */
 void dpm_resume_end(pm_message_t state)
 {
-	pm_restore_gfp_mask();
 	dpm_resume(state);
 	pm_restore_gfp_mask();
 	dpm_complete(state);
